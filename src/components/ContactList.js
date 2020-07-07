c2c030ffd58688a0a2e2e186c7043095e34a2601
pashadidychenko <pasha.didychenko@gmail.com>
2020-07-07 12:21:46 +0300

    Great home work

---
+++ src/components/ContactList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ContactListItem from "./ContactListItem";
+import PropTypes from "prop-types";
+
+const ContactList = ({ contactList, deleteContact }) => {
+  return (
+    <>
+      <ul>
+        {contactList.map((contact) => (
+          <ContactListItem
+            deleteContact={() => deleteContact(contact.id)}
+            contact={contact}
+            key={contact.id}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+  contactList: PropTypes.array,
+};
